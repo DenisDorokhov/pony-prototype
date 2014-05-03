9d218c8a6fd0d2d0d6277f189514da85dda0cd7a
DenisDorokhov <denis@dorokhov.net>
2014-05-03 13:59:10 +0200

    show settings window automatically when no data is available

--- source/pony.web/src/main/java/net/dorokhov/pony/web/client/mvp/common/ToolbarPresenter.java
+++ source/pony.web/src/main/java/net/dorokhov/pony/web/client/mvp/common/ToolbarPresenter.java
@@ -5,22 +5,35 @@ import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.HasUiHandlers;
 import com.gwtplatform.mvp.client.PresenterWidget;
 import com.gwtplatform.mvp.client.View;
+import net.dorokhov.pony.web.client.event.NoDataEvent;
 import net.dorokhov.pony.web.client.event.RefreshEvent;
+import net.dorokhov.pony.web.client.service.LibraryScanner;
 
-public class ToolbarPresenter extends PresenterWidget<ToolbarPresenter.MyView> implements ToolbarUiHandlers {
+public class ToolbarPresenter extends PresenterWidget<ToolbarPresenter.MyView> implements ToolbarUiHandlers, NoDataEvent.Handler {
 
 	public interface MyView extends View, HasUiHandlers<ToolbarUiHandlers> {}
+
+	private final LibraryScanner libraryScanner;
 
 	private final SettingsPresenter settingsPresenter;
 
 	@Inject
-	public ToolbarPresenter(EventBus aEventBus, MyView aView, SettingsPresenter aPresenter) {
+	public ToolbarPresenter(EventBus aEventBus, MyView aView, LibraryScanner aLibraryScanner, SettingsPresenter aPresenter) {
 
 		super(aEventBus, aView);
 
+		libraryScanner = aLibraryScanner;
 		settingsPresenter = aPresenter;
 
 		getView().setUiHandlers(this);
+	}
+
+	@Override
+	protected void onBind() {
+
+		super.onBind();
+
+		addRegisteredHandler(NoDataEvent.NO_DATA_DETECTED, this);
 	}
 
 	@Override
@@ -30,6 +43,19 @@ public class ToolbarPresenter extends PresenterWidget<ToolbarPresenter.MyView> i
 
 	@Override
 	public void onSettingsRequested() {
-		addToPopupSlot(settingsPresenter);
+		showSettings();
+	}
+
+	@Override
+	public void onNoDataEvent(NoDataEvent aEvent) {
+		if (!libraryScanner.isScanning()) {
+			showSettings();
+		}
+	}
+
+	private void showSettings() {
+		if (!settingsPresenter.isVisible()) {
+			addToPopupSlot(settingsPresenter);
+		}
 	}
 }
